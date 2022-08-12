b2620a951c6236ad2a9df6e1914dfd7e0c4ac47d
pabloskyDev <jpbetancur1999@gmail.com>
2022-08-12 10:38:40 -0500

    Organizar estructura del index y archivo javascript

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function cargarScrollRevealIndex(sr) {
+    sr.reveal('#carousel-index',{
+        duration:2000,
+        origin: 'left',
+        distance:'800px'
+    });
+    
+    sr.reveal('.card1',{
+        duration:2000,
+        origin: 'top',
+        distance:'200px'
+    });
+    
+    sr.reveal('.card2',{
+        duration:2000,
+        origin: 'bottom',
+        distance:'200px'
+    });
+    
+    sr.reveal('.card3',{
+        duration:2000,
+        origin: 'top',
+        distance:'200px'
+    });
+}
+
+function textWrapperIndex() {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+   .add({
+        targets: '.ml .letter',
+        scale: [0, 1],
+        duration: 2500,
+        elasticity: 800,
+        delay: (el, i) => 100 * (i+1)
+    }).add({
+        targets: '.ml',
+        opacity: 0,
+        duration: 2000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+}
